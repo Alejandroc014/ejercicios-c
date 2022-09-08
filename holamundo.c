6cc6e98bd1a82f076eff1e6bf8ca2301cda2274e
Alejandro <alejandro@email.com>
2022-09-08 13:26:09 +0200

    Edad para entrar en el casino

--- holamundo.c
+++ holamundo.c
@@ -4,6 +4,14 @@ int main()
 {
     /*tu codigo aqui*/
     printf("Hola Mundo desde linux y c");
+    scanf("%d", &edad);
+
+    if (edad > 17){
+        /*imprimimos en pantalla */
+        printf("Bienvenido %s, tienes %d años.\n", nombre, edad);
+    }else if(edad ==17){
+        printf("Te esperamos, casi tienes 18!!");
+    }
     return 0;
 }
 
